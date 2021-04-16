7a145b69a9a9183de9e899db5d9a220b75d25b81
elizeurs <13ders@gmail.com>
2021-04-16 13:09:57 -0300

    Initial Commit

---
+++ StocksAppSwiftUI/StocksAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksAppSwiftUIApp.swift
+//  StocksAppSwiftUI
+//
+//  Created by Elizeu RS on 16/04/21.
+//
+
+import SwiftUI
+
+@main
+struct StocksAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
