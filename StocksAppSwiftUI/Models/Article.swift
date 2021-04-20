82857c637094030ed9413aca46a6c45a0d1290ee
elizeurs <13ders@gmail.com>
2021-04-20 11:38:29 -0300

    Implementing Webservice to Fetch News and Creating Models

---
+++ StocksAppSwiftUI/Models/Article.swift
@@ -0,0 +1,14 @@
+//
+//  Article.swift
+//  StocksAppSwiftUI
+//
+//  Created by Elizeu RS on 19/04/21.
+//
+
+import Foundation
+
+struct Article: Decodable {
+  let title: String
+  let imageURL: String
+  let publication: String
+}
